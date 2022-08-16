f23a3c770a6c3d1d476e9c334a6fa69cd1f16f2f
Jonathan Kupcho <jonathan.kupcho@healthcatalyst.com>
2022-08-16 02:09:15 +0000

    episode 5 start

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Home</h1>
+    <Nav></Nav>
+    <p>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+    </p>
+</template>
+
+<script>
+import Nav from "../Shared/Nav"
+export default {
+    components: { Nav },
+    props: {
+        name: String,
+        frameworks: Array
+    }
+}
+</script>
---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Users</h1>
+    <Nav></Nav>
+</template>
+
+<script>
+import Nav from "../Shared/Nav"
+export default {
+    components: { Nav },
+}
+</script>
